d4cebc25838a098fcb5242ae84e7d7edba859fa7
Nikola Kristanović <nikola.kristanovic@gmail.com>
2022-02-03 17:04:56 +0100

    Fixed slider

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 $(document).ready(function () {
-    let item_widthFirstRow = $('.content__slider-firstRow .slide').outerWidth();
-    let item_widthSecondRow = $('.content__slider-secondRow .slide').outerWidth();
 
     $('#next').click(function () {
+        let item_widthFirstRow = $('.content__slider-firstRow .slide:last').width();
+        let left_valueFirstRow = item_widthFirstRow * (-1);
 
-        $('.content__slider-firstRow').animate({ 'right': item_widthFirstRow }, 300, function () {
-            $('.content__slider-firstRow .slide:first').before($('.content__slider-firstRow .slide:last'));
+        let item_widthSecondRow = $('.content__slider-secondRow .slide:last').width();
+        let left_valueSecondRow = item_widthSecondRow * (-1);
+
+        $('#next').addClass('disabled');
+
+        $('.content__slider-firstRow').animate({ 'left': '+=' + item_widthFirstRow }, 2000, function () {
+            $('.content__slider-firstRow .slide:last').prependTo($('.content__slider-firstRow'));
+            $('.content__slider-firstRow').css({ 'left': '+=' + left_valueFirstRow });
+            $('#next').removeClass('disabled');
         });
 
-        $('.content__slider-secondRow').animate({ 'right': item_widthSecondRow }, 300, function () {
-            $('.content__slider-secondRow .slide:first').before($('.content__slider-secondRow .slide:last'));
+        $('.content__slider-secondRow').animate({ 'left': '+=' + item_widthSecondRow }, 2000, function () {
+            $('.content__slider-secondRow .slide:last').prependTo($('.content__slider-secondRow'));
+            $('.content__slider-secondRow').css({ 'left': '+=' + left_valueSecondRow });
         });
     });
 
     $('#prev').click(function () {
-        $('.content__slider-firstRow').animate({ 'left': item_widthFirstRow }, 300, function () {
-            $('.content__slider-firstRow .slide:last').after($('.content__slider-firstRow .slide:first'));
+        let item_widthFirstRow = $('.content__slider-firstRow .slide:first').width();
+        let left_valueFirstRow = item_widthFirstRow * (-1);
+
+        let item_widthSecondRow = $('.content__slider-secondRow .slide:first').width();
+        let left_valueSecondRow = item_widthSecondRow * (-1);
+
+        $('#prev').addClass('disabled');
+
+        $('.content__slider-firstRow ').animate({ 'left': '-=' + item_widthFirstRow }, 2000, function () {
+            $('.content__slider-firstRow .slide:first').appendTo($('.content__slider-firstRow'));
+            $('.content__slider-firstRow').css({ 'left': '-=' + left_valueFirstRow });
+            $('#prev').removeClass('disabled');
         });
 
-        $('.content__slider-secondRow').animate({ 'left': item_widthSecondRow }, 300, function () {
-            $('.content__slider-secondRow .slide:last').after($('.content__slider-secondRow .slide:first'));
+        $('.content__slider-secondRow').animate({ 'left': '-=' + item_widthSecondRow }, 2000, function () {
+            $('.content__slider-secondRow .slide:first').appendTo($('.content__slider-secondRow'));
+
+            $('.content__slider-secondRow').css({ 'left': '-=' + left_valueSecondRow });
 
         });
     });
