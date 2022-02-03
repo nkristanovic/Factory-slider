a2fd6324e99022a770485c1fc1d85a3387353f5f
Nikola Kristanović <nikola.kristanovic@gmail.com>
2022-02-03 17:16:41 +0100

    Fixed slider

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ $(document).ready(function () {
         let item_widthSecondRow = $('.content__slider-secondRow .slide:last').width();
         let left_valueSecondRow = item_widthSecondRow * (-1);
 
-        $('#next').addClass('disabled');
+        $('.btn').addClass('disabled');
 
         $('.content__slider-firstRow').animate({ 'left': '+=' + item_widthFirstRow }, 2000, function () {
             $('.content__slider-firstRow .slide:last').prependTo($('.content__slider-firstRow'));
             $('.content__slider-firstRow').css({ 'left': '+=' + left_valueFirstRow });
-            $('#next').removeClass('disabled');
+            $('.btn').removeClass('disabled');
         });
 
         $('.content__slider-secondRow').animate({ 'left': '+=' + item_widthSecondRow }, 2000, function () {
@@ -28,12 +28,12 @@ $(document).ready(function () {
         let item_widthSecondRow = $('.content__slider-secondRow .slide:first').width();
         let left_valueSecondRow = item_widthSecondRow * (-1);
 
-        $('#prev').addClass('disabled');
+        $('.btn').addClass('disabled');
 
         $('.content__slider-firstRow ').animate({ 'left': '-=' + item_widthFirstRow }, 2000, function () {
             $('.content__slider-firstRow .slide:first').appendTo($('.content__slider-firstRow'));
             $('.content__slider-firstRow').css({ 'left': '-=' + left_valueFirstRow });
-            $('#prev').removeClass('disabled');
+            $('.btn').removeClass('disabled');
         });
 
         $('.content__slider-secondRow').animate({ 'left': '-=' + item_widthSecondRow }, 2000, function () {
